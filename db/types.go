09703b8cb6d5ae832a1c525651219540d6c9543a
Aliaksei Mazur <MazurA@iba>
2019-07-11 11:50:34 +0300

    added db types

---
+++ db/types.go
@@ -0,0 +1,42 @@
+package db
+
+type Route struct {
+  Instance int
+  RouteId  int
+  Type     int
+}
+
+type Station struct {
+  StationId int
+  Instance  int
+  Type      int
+  TitleRu   string
+  TitleEn   string
+  TitleBy   string
+  Direction int
+  Lat       float64
+  Lng       float64
+}
+
+type Point struct {
+  PointId   int
+  Instance  int
+  StationId int
+  RouteId   int
+  Direction int
+  Distance  int
+  Lat       float64
+  Lng       float64
+}
+
+type Schedule struct {
+  ScheduleId int
+  Instance   int
+  RouteId    int
+  StationId  int
+  TripId     int
+  Time       int
+  Weekdays   int
+  CardNum    int
+  FileId     int
+}
