59beec1fa00fdfad59acb2e33be3ec597c1a7027
Alex Mazur <aliaksei.mazur@iba.by>
2019-09-03 17:32:45 +0300

    added overpass utils

---
+++ overpass/overpass.go
@@ -0,0 +1,32 @@
+package overpass
+
+import . "github.com/Nivelian/avm/utils"
+
+const URL = "https://lz4.overpass-api.de/api/interpreter"
+
+type OsmTags struct {
+  Name    string
+  Colour  string
+}
+type OsmMember struct {
+  Type string
+  Role string
+  Ref  int
+}
+type OsmElement struct {
+  Id       int
+  Type     string
+  Nodes    []int
+  Lat      float64
+  Lon      float64
+  Tags     OsmTags
+  Elements []OsmElement
+  Members  []OsmMember
+}
+
+func LoadData (file string) []OsmElement {
+  var res OsmElement
+  args := map[string]interface{}{"data": string(LocalFile( Fmt("{0}.ovps", file) ))}
+  FromJson(&res, RemoteFile(URL, nil, args))
+  return res.Elements
+}
