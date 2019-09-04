9bd7d1ca7f56ebe8c63225ab908df9e5b666a66c
Alex Mazur <aliaksei.mazur@iba.by>
2019-09-04 12:51:37 +0300

    changed OsmElement model

--- overpass/overpass.go
+++ overpass/overpass.go
@@ -1,6 +1,8 @@
 package overpass
 
-import . "github.com/Nivelian/avm/utils"
+import (
+  . "github.com/Nivelian/avm/utils"
+)
 
 const URL = "https://lz4.overpass-api.de/api/interpreter"
 
@@ -32,4 +34,19 @@ func LoadData (file string) []OsmElement {
   args := map[string]interface{}{"data": string(LocalFile( Fmt("{0}.ovps", file) ))}
   FromJson(&res, RemoteFile(URL, nil, args))
   return res.Elements
+}
+
+func (el OsmElement) reverse () (res OsmElement) {
+  res = DeepCopy(el).(OsmElement)
+  res.Elements = []OsmElement{}
+  for i := len(el.Elements)-1; i >=0; i-- {res.Elements = append(res.Elements, el.Elements[i])}
+  return
+}
+
+func (el OsmElement) flatten () (res OsmElement) {
+  res = DeepCopy(el).(OsmElement)
+  var nodes []OsmElement
+  for _, way := range el.Elements {nodes = append(nodes, way.Elements...)}
+  res.Elements = nodes
+  return
 }
